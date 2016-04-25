cdba1f3fe18071e1fae82d7d3b2bcb5295b166a7
Rohan Elukurthy <roh3@pdx.edu>
2016-04-24 18:06:31 -0700

    adding Rudd's files

---
+++ CS300_TermProject_ChocAn/src/com/company/Member.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by hERO on 4/24/16.
+ */
+public class Member {
+    private String name;
+    private int memNumber;
+    private String memAddress;
+    private String city;
+    private String state;
+    private int memZip;
+    private Service serviceList;
+}
---
+++ CS300_TermProject_ChocAn/src/com/company/Service.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by hERO on 4/24/16.
+ */
+public class Service {
+    private int dateService;
+    private String providerName;
+    private String serviceName;
+}
