b6b1054fe88f8f2059458ebcf345c9873ced97e6
timothy k handojo <thandojo@pdx.edu>
2016-05-08 18:56:21 -0700

    changed funcs to private

--- CS300_TermProject_ChocAn/src/com/company/Main.java
+++ CS300_TermProject_ChocAn/src/com/company/Main.java
@@ -10,6 +10,6 @@ public class Main {
     }
     public static void main(String[] args) {}
 
-    public Customer loginCustomer (String login) {}
-    public Customer loginProvider (String login) {}
+    private Customer loginCustomer (String login) {}
+    private Customer loginProvider (String login) {}
 }
