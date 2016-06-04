26bd55cb1170860daf25a262fdec54b60e227098
Rohan Elukurthy <roh3@pdx.edu>
2016-06-03 23:30:46 -0700

    Working fileWriter

---
+++ CS300_TermProject_ChocAn/src/com/company/fileWriter.java
@@ -0,0 +1,31 @@
+package company;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by hERO on 6/3/16.
+ */
+public class fileWriter {
+    String filename = null;
+
+    public fileWriter(){
+        filename = null;
+    }
+
+    public fileWriter(String filename){
+        this.filename = filename;
+    }
+
+    public void write_to_file(String []writeData){
+        try {
+            PrintWriter writer = new PrintWriter(filename, "UTF-8");
+            writer.close();
+        }catch (UnsupportedEncodingException f){
+            f.getMessage();
+        }catch (FileNotFoundException f){
+            f.getMessage();
+        }
+    }
+}
